ea708064c9b4cb4852e2d8bb5e647496dcfaa01b
siunix <abd.siunix@gmail.com>
2021-11-20 02:33:54 +0700

    Advice generator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
+import './App.css'
+
+export default function App() {
+
+  const [advice, setAdvice] = useState("")
+
+  const fetchData = () => {
+    axios.get(`${process.env.REACT_APP_API_URL}/advice`)
+      .then((response) => {
+        setAdvice(response.data.slip.advice)
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  // Note : If you want to run an effect and clean it up only once (on mount and unmount), you can pass an empty array ([]) as a second argument.
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="card">
+        <h1 className="heading">{advice}</h1>
+        <button className="button" onClick={fetchData}>
+          <span>GET NEW ONE!</span>
+        </button>
+      </div>
     </div>
-  );
+  )
 }
-
-export default App;
